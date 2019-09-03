8c806f51d80486ce8f5e905da19f4a23adee7417
Jairo <jjriosc90@gmail.com>
2019-09-02 21:26:43 -0500

    Clase 2 - vectores

---
+++ Vectores/Assets/Scripts/Cube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    Vector3 velocity = new Vector3(-3, 4, 0);
+    Vector3 acceleration = new Vector3(0, -9.8f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += velocity * Time.deltaTime;
+        velocity += acceleration * Time.deltaTime;
+
+        if (transform.position.y <-5){
+            velocity.y *= -1;
+        }
+
+        if (transform.position.x <-9 || transform.position.x > 9){
+            velocity.x *= -1;
+        }
+
+    }
+}
